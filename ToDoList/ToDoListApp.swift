bc234bb1e081f2b18702884fd3c10bdf801e742c
krish962 <balakrishnan.logicfocus@gmail.com>
2023-09-29 20:05:04 +0530

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Logicfocus Techonology Solution on 29/09/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
